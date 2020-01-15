e33ee547ecc2ec91b065b74bfdb21bc84efc5da4
czarny <filip.sawicki85@gmail.com>
2020-01-15 23:11:57 +0100

    zadania

---
+++ ile_parzystych.py
@@ -0,0 +1,36 @@
+"""
+    Zadanie polega na napisaniu funkcji zliczajacej ilosc
+    parzystych liczb w liscie liczb podanej jako parametr funkcji.
+    Funkcja nazywa sie count_even_number, natomiast parametr wejsciowy
+    jest lista liczb o nazwie numbers.
+    Napisz rozwiazanie, ktore przebada liste w poszukiwaniu liczb parzystych
+    i przy kazdym znalezieniu takowej zwiekszy licznik zmiennej pomocniczej.
+    Funkcja powinna zwracac ilosc liczb parzystych w podanej liscie.
+    Zakladamy, ze lista bedzie dostarczana z programu, nie przez uzytkownika
+    z wykorzystaniem funkcji input().
+    Przyklad:
+    >> list_of_numbers = [1, 2, 3, 4, 5, 6] -> 3 liczby parzyste: 2, 4, 6
+    >> x = count_even_numbers(list_of_numbers)
+    x powinno wynosic 3.
+    Uwaga: 0 jest liczba parzysta.
+    Zlicza liczby parzyste w liscie podanej jako argument.
+    :param numbers: lista liczb.
+    :return: ilosc liczb parzystych w podanej liscie, typ wartosci
+        zwracanej to int.
+
+"""
+
+
+def count_even_numbers(numbers):
+
+    lx = []
+    for i in numbers:
+        if (i % 2) == 0:
+            lx.append(i)
+    x = len(lx)
+    return x
+
+
+
+
+
