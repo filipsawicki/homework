b533b539794f475bbfee6ba4a86c94b93f68fdf7
czarny <filip.sawicki85@gmail.com>
2020-01-24 23:27:21 +0100

    next done

--- gra_w_wojne.py
+++ gra_w_wojne.py
@@ -17,13 +17,20 @@
     1
     >> determine_the_winner("4", "4")
     0
+    Wskazuje zwyciezce potyczki w grze wojna.
+    :param card1: litera (string) reprezentujaca karte gracza 1.
+    :param card2: litera (string) reprezentujaca karte gracza 2.
+    :return: 0 dla remisu, 1 jesli zwyciezy gracz 1, 2 dla zwyciestwa gracza 2.
 """
 
 
 def determine_the_winner(card1, card2):
-    """Wskazuje zwyciezce potyczki w grze wojna.
-    :param card1: litera (string) reprezentujaca karte gracza 1.
-    :param card2: litera (string) reprezentujaca karte gracza 2.
-    :return: 0 dla remisu, 1 jesli zwyciezy gracz 1, 2 dla zwyciestwa gracza 2.
-    """
-    pass
+    cards_dict = {'2': 1, '3': 2, '4': 3, '5': 4, '6': 5, '7': 6, '8': 7, '9': 8, '10': 9,'J': 10, 'D': 11, 'K': 12, 'A': 13}
+    if card1 > card2:
+        return 1
+    elif card2 > card1:
+        return 2
+    return 0
+
+
+print(determine_the_winner("8", "A"))
