f65882651e4bea1c935c64921595daa3d1ac206a
czarny <filip.sawicki85@gmail.com>
2020-02-23 23:03:10 +0100

    update!

--- hometask_1/meal/baza/meal.py
+++ hometask_1/meal/baza/meal.py
@@ -18,22 +18,25 @@ class Meal:
         Meal.sum_cal -= self.kalorie
         del (self.posilek, self.kalorie)
 
-    # setter - dodajemy posilek oraz jego kalorie,
+    # metoda setter - dodajemy posilek oraz jego kalorie,
     def set_posilek(self, posilek, kalorie):
         self.posilek = posilek
         self.kalorie = kalorie
         Meal.sum_cal += kalorie
         Meal.meal_dict.update({self.posilek: self.kalorie})
 
+    # metoda getter - wyswietla posilek oraz jego kalorie
     def get_posilek(self):
-        return print(f" Posilek:  {self.posilek} - Kalorie: {self.kalorie}")
+        return print(f"Posilek:  {self.posilek} - Kalorie: {self.kalorie}")
 
 
 
 
+
+    # funkcja wyswietla sume wszystkich kalorii
 def get_sum_calories():
-    return print(f'Suma kalorii wszystkich posilkow {Meal.sum_cal}')
+    return print(f' Suma kalorii wszystkich posilkow {Meal.sum_cal}')
 
-
+    # funkcja wyswietla slownik ze wszystkimi posilkami
 def get_dict():
-    return print(f'Słownik wszystkich posilkow {Meal.meal_dict}')
+    return print(f' Słownik wszystkich posilkow {Meal.meal_dict}')
--- hometask_1/meal/main.py
+++ hometask_1/meal/main.py
@@ -1,21 +1,24 @@
 from hometask_1.meal.baza.meal import Meal, get_sum_calories, get_dict
 
-spaghetty = Meal()
-spaghetty.set_posilek("Spaghetty", 220)
+spaghetti = Meal()
+spaghetti.set_posilek("Spaghetti", 220)
 
 steak = Meal()
 steak.set_posilek("Steak", 80)
 
-eaggs = Meal()
-eaggs.set_posilek("Eaggs", 50)
+eggs = Meal()
+eggs.set_posilek("Eggs", 50)
 
 get_sum_calories()
 
 
-Meal.delete_meal(steak)
-
 get_sum_calories()
 
-spaghetty.get_posilek()
+spaghetti.get_posilek()
 
 get_dict()
+
+
+steak.get_posilek()
+
+
