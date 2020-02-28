e3d7dd9291616467f1b5b319c69f1104539cb669
dedi <filip.sawicki85@gmail.com>
2020-02-28 23:19:50 +0100

    next task

--- homework_2/wielokaty.py
+++ homework_2/wielokaty.py
@@ -1,13 +1,3 @@
-"""
-    Na podstawie klasy abstrakcyjnej Wielokat, stworz 3 klasy dziedziczace: Kwadrat, Prostokat, Trojkat.
-    Kazda z tych klas musi byc konkretna (a nie abstrakcyjna), powinna wiec nadpisywac wszystkie metody
-    abstrakcyjne z klasy Wielokat. Kazda konkretna klasa powinna miec definicje konstruktora __init__
-    (dla przykładu obiekt typu Kwadrat, powinien miec jeden atrybut self.bok, obiekt prostokat dwa atrybuty:
-    self.bok1, self.bok2, a trojkat self.bok, self.wysokosc).
-    Na koniec stworz funkcje zarzadzajaca obiektami stworzonych klas, ktora w petli bedzie obliczala pola
-    dla kazdego obiektu.
-"""
-
 from abc import ABC, abstractmethod
 from typing import Union
 
@@ -16,29 +6,40 @@ class Wielokat(ABC):
 
     @abstractmethod
     def oblicz_pole(self) -> Union[int, float]:  # Union sugeruje ze funkcja moze zwrocic inta albo floata
-        """Metoda ma za zadanie obliczyc pole danego obiektu.
-        Ze wzgledu na fakt, iz nie wiemy w tym momencie jaki to jest wielokat,
-        nie potrafimy obliczyc jego pola. Do redefinicji w klasie dziedziczacej.
-        :return: wartosc pola jako liczba calkowita lub wymierna.
-        """
-        ...
-
+        pass
 
 class Kwadrat(Wielokat):
-    pass
+    def __init__(self, bok):
+        self.bok = bok
+
+    def oblicz_pole(self) -> Union[int, float]:
+        return self.bok ** 2
 
 
 class Prostokat(Wielokat):
-    pass
+    def __init__(self, bok1, bok2):
+        self.bok1 = bok1
+        self.bok2 = bok2
+
+    def oblicz_pole(self) -> Union[int, float]:
+        return self.bok1 * self.bok2
 
 
 class Trojkat(Wielokat):
-    pass
+    def __init__(self, bok, wysokosc):
+        self.bok = bok
+        self.wysokosc = wysokosc
+
+    def oblicz_pole(self) -> Union[int, float]:
+        return 1 / 2 * self.bok * self.wysokosc
 
 
 def licz_pola(wielokaty: list) -> list:
-    """Oblicza pola wszystkich wielokatow podanych w liscie.
-    :param wielokaty: lista obiektow dziedziczacych po Wielokat.
-    :return: lista obliczonych pol.
-    """
-    pass
+    list_area = []
+
+    for elements in wielokaty:
+        list_area.append((elements.oblicz_pole()))
+
+    return list_area
+
+
